32e6a6a4eb596cf55233151fbdf24bcc9fc7f752
leemjn <leemjn>
2022-08-10 15:48:48 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path,include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('blog/',include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    Posts = Post.objects.all()
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':Posts,
+        }
+    )
