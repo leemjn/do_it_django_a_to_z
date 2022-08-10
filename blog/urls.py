b3ca8f561d0a8db382a13215647036410d2291c3
leemjn <leemjn>
2022-08-10 16:28:27 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
+    path('<int:pk>/', views.single_post_page),
+    path('', views.index),
 ]
