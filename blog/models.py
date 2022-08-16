63e54ab97fc2c8ed6f7c47d67538e1a59b1bb32a
leemjn <leemjn>
2022-08-16 16:24:03 +0900

    포스트목록 페이지 카테고리 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 import os
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True,
+                            allow_unicode=True)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 class Post(models.Model):
     title=models.CharField(max_length=50)
     hook_text=models.CharField(max_length=100,blank=True)
@@ -13,6 +23,7 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
@@ -26,3 +37,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
